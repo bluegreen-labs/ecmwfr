882f70619c8921ab258dd7ab9f1b20ceb5239031
khufkens <koen.hufkens@gmail.com>
2019-01-01 16:21:40 +0100

    unit tests

--- tests/testthat/test_main_functions.r
+++ tests/testthat/test_main_functions.r
@@ -1,17 +1,73 @@
-# test functions without parameters
-# only can fail upon server error
-
+# set options
 opts <- options(keyring_warn_for_env_fallback = FALSE)
 on.exit(options(opts), add = TRUE)
 
-test_that("set, get", {
+# format request (see below)
+my_request <- list(stream = "oper",
+                   levtype = "sfc",
+                   param = "165.128",
+                   dataset = "interim",
+                   step = "0",
+                   grid = "0.75/0.75",
+                   time = "00",
+                   date = "2014-07-01/to/2014-07-31",
+                   type = "an",
+                   class = "ei",
+                   area = "73.5/-27/33/45",
+                   format = "netcdf",
+                   target = "tmp.nc")
 
-  skip_on_cran()
+# set password
+key <- system("echo $KEY", intern = TRUE)
+wf_set_key(email = "koenhufkens@gmail.com",
+           key = key)
 
-  username <- "joe@example.com"
-  password <- "horriblepassword"
-
-  expect_silent(wf_set_key(username, password))
-  expect_equal(wf_get_key(username), password)
+# check returned decrypted key
+test_that("set, get secret key", {
+  #skip_on_cran()
+  expect_equal(wf_get_key(email = "koenhufkens@gmail.com"), key)
 })
 
+test_that("test dataset function", {
+  #skip_on_cran()
+  expect_output(str(wf_datasets(email = "koenhufkens@gmail.com")))
+})
+
+test_that("test dataset function - no login", {
+  skip_on_cran()
+  expect_error(wf_datasets())
+})
+
+test_that("test services function", {
+  #skip_on_cran()
+  expect_output(str(wf_services(email = "koenhufkens@gmail.com")))
+})
+
+test_that("test services function - no login", {
+  #skip_on_cran()
+  expect_error(wf_services())
+})
+
+test_that("test user info function", {
+  #skip_on_cran()
+  expect_output(str(wf_user_info(email = "koenhufkens@gmail.com")))
+})
+
+test_that("test user info function - no login", {
+  #skip_on_cran()
+  expect_error(wf_user_info())
+})
+
+test_that("test request (transfer) function", {
+  #skip_on_cran()
+  expect_message(wf_request(
+    email = "koenhufkens@gmail.com",
+    transfer = TRUE,
+    request = my_request,
+    time_out = 60))
+})
+
+test_that("test transfer function - no login", {
+  #skip_on_cran()
+  expect_error(wf_transfer())
+})

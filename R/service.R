b2a50ff2b11406a742989dad95ef48d2768ecd0c
Elio Campitelli <eliocampitelli@gmail.com>
2021-03-15 21:47:30 -0300

    Makes services non clonable
    This is so that they are always in sync with the web service.

--- R/service.R
+++ R/service.R
@@ -1,4 +1,4 @@
-service <- R6::R6Class("ecmwfr_service",
+service <- R6::R6Class("ecmwfr_service", cloneable = FALSE,
   public = list(
     initialize = function(request,
                           user,

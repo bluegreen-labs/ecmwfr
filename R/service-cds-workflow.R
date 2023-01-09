b74f340db5fcc32f9ca363cebe3466db9dccf1fc
Elio Campitelli <eliocampitelli@gmail.com>
2023-01-09 15:18:46 -0300

    Implements cds workflow

---
+++ R/service-cds-workflow.R
@@ -0,0 +1,17 @@
+cds_workflow <- R6::R6Class("ecmwfr_cds_workflow", inherit = cds_service,
+  private = list(
+    http_verb = "PUT",
+    request_url = function() {
+      sprintf(
+        "%s/tasks/services/%s/clientid-%s",
+        wf_server(service = "cds"),
+        # NOTE THE DIFFERENENT ENDPOINT FOR TOOLBOX EDITOR APPS
+        gsub("\\.", "/","tool.toolbox.orchestrator.run_workflow"),
+        uuid::UUIDgenerate(output = "string")
+      )
+    },
+    get_location = function(content) {
+      content$result[[1]]$location
+    }
+  )
+)

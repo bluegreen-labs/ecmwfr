0e88ffee7b48c0116e16d45bf298816ee54a3c9a
Koen Hufkens <koen.hufkens@gmail.com>
2023-01-10 23:52:38 +0100

    update workflow test

--- analysis/test_run_workflow.R
+++ analysis/test_run_workflow.R
@@ -1,7 +1,29 @@
 library(ecmwfr)
 
-code <- readLines("analysis/test.py") |>
-  paste0(collapse = "\n")
+code <-
+"
+import cdstoolbox as ct
+
+@ct.application()
+@ct.output.download()
+def plot_time_series(var, lon, lat):
+    data = ct.catalogue.retrieve(
+      'reanalysis-era5-single-levels',
+      {
+        'variable': '2m_temperature',
+        'grid': ['3', '3'],
+        'product_type': 'reanalysis',
+        'year': ['2008'],
+        'month': ['01'],
+        'day': ['01'],
+        'time': ['00:00', '06:00', '12:00', '18:00'],
+      }
+    )
+
+    data_sel = ct.geo.extract_point(data, lon=lon, lat=lat)
+    data_daily = ct.climate.daily_mean(data_sel)
+    return data_daily
+"
 
 # A query for 2m surface temperature
 request = list(

eb278539072d2035773b3cc1883b9bbf1e113a9f
KH <koen.hufkens@gmail.com>
2024-08-21 16:42:34 +0200

    updating legacy independent functions

--- R/wf_delete.R
+++ R/wf_delete.R
@@ -1,13 +1,10 @@
-#' delete ECMWF request
+#' Delete ECMWF Data Store request
 #'
-#' Deletes a staged download from the queue
+#' Deletes a staged download from the queue when not using R6 methods.
 #'
-#' @param user user (email address) used to sign up for the ECMWF data service,
-#' used to retrieve the token set by \code{\link[ecmwfr]{wf_set_key}}
 #' @param url url to query
+#' @param user user, generally not set (default = "ecmwfr"), used by \code{\link[ecmwfr]{wf_set_key}}
 #' @param verbose show feedback on processing
-#' @param service which service to use, one of \code{cds}
-#' or \code{ads} (default = webapi)
 #' @seealso \code{\link[ecmwfr]{wf_set_key}}
 #' \code{\link[ecmwfr]{wf_transfer}}
 #' \code{\link[ecmwfr]{wf_request}}
@@ -16,57 +13,41 @@
 #' @examples
 #'
 #' \dontrun{
-#' # set key
-#' wf_set_key(email = "test@mail.com", key = "123")
-#'
-#' # get key
-#' wf_get_key(email = "test@mail.com")
+#' # delete request
+#' job_url <- file$get_url()
+#' wf_delete(url = job_url)
 #'}
 
 wf_delete <- function(
   url,
-  user,
-  service = "webapi",
+  user = "ecmwfr",
   verbose = TRUE
 ){
 
   # check the login credentials
-  if(missing(user) | missing(url)){
+  if(missing(url)){
     stop("Please provide ECMWF login user / url!")
   }
 
-  # match arguments, if not stop
-  service <- match.arg(service, c("cds", "ads"))
-
   # get key
-  key <- wf_get_key(user = user, service = service)
+  key <- wf_get_key(user = user)
 
-  # If the URL is not an URL but an ID: generate URL
-  if (service == "cds" | service == "ads") {
-    url <- wf_server(id = url, service = service)
-  }
-
-  # remove a queued download
-  # Differs for ecmwf and cds requests.
-  # For CDS: note that 'user' is simply a copy of 'user'
-  if(service == "cds" | service == "ads") {
-    response <- httr::DELETE(
-      url,
-      httr::authenticate(user, key),
-      httr::add_headers(
-        "Accept" = "application/json",
-        "Content-Type" = "application/json")
+  #  get the response for the query provided
+  response <- httr::DELETE(
+    url,
+    httr::add_headers(
+      "PRIVATE-TOKEN" = key
     )
-  }
+  )
 
-  # check purging of request
-  if(response$status == 204){
+  # trap general http error, otherwise report success
+  if (httr::http_error(response)) {
+    warning("Request not purged from queue, check download!")
+  } else {
     if (verbose){
       message("- request purged from queue!")
     } else {
       invisible()
     }
-  } else {
-    warning("Request not purged from queue, check download!")
   }
 }

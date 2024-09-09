bcc6b65c0df8dda9f477d8453d5dc60926151fa9
Raphaël Nussbaumer <rafnuss@gmail.com>
2024-09-09 08:08:39 +0200

    remove assignment `NULL` to missing user

--- R/wf_request_batch.R
+++ R/wf_request_batch.R
@@ -38,7 +38,6 @@ wf_request_batch <- function(
 
   force(total_timeout)  # Need to evaluate the expression before changing time_out
   time_out <- repeat_if_one(time_out, N)
-  if (missing(user)) user <- NULL
 
   user <- repeat_if_one(user, N)
   path <- repeat_if_one(path, N)
